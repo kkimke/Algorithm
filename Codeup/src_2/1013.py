9f6e32cb97ac03966e4d02c4ee6dbe9e7fddc587
kkimke <k_k_eun@naver.com>
2021-01-30 13:54:28 +0900

    codeup src_2

---
+++ Codeup/src_2/1013.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+
+print(a, b)
+
+
+'''
+a,b = input().split()
+n=int(a)
+m=int(b)
+print(a, b)
+'''
---
+++ Codeup/src_2/1017.py
@@ -0,0 +1,10 @@
+a = str(input())
+
+print((a + " ") * 3)
+
+
+'''
+n=input()
+n=int(n)
+print(n, n, n,);
+'''
