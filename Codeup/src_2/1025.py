66573a9943959c8a8d4d1fcbe4d72668676998dd
kkimke <k_k_eun@naver.com>
2021-02-02 23:52:51 +0900

    codeup from 1025 to 1027

---
+++ Codeup/src_2/1025.py
@@ -0,0 +1,17 @@
+s = input()
+
+print([int(s[0]) * 10000])
+print([int(s[1]) * 1000])
+print([int(s[2]) * 100])
+print([int(s[3]) * 10])
+print([int(s[4])])
+
+'''
+n=input()
+
+print("["+str(int(n[0])*10000)+"]")
+print("["+str(int(n[1])*1000)+"]")
+print("["+str(int(n[2])*100)+"]")
+print("["+str(int(n[3])*10)+"]")
+print("["+str(int(n[4]))+"]")
+'''
---
+++ Codeup/src_2/1027.py
@@ -0,0 +1,11 @@
+y, m, d = input().split('.')
+
+print(d, m, y, sep='-')
+
+'''
+y,m,d=input().split('.')
+
+print('%02d' % int(d), end='-')
+print('%02d' % int(m), end='-')
+print('%04d' % int(y))
+'''
