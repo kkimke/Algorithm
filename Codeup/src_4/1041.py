6d54a82333dda0d4cf88943461a7e76cd539f82a
kkimke <k_k_eun@naver.com>
2021-02-06 14:27:15 +0900

    codeup from 1038 to 1046

---
+++ Codeup/src_4/1041.py
@@ -0,0 +1,5 @@
+a = input()
+a = ord(a)
+
+b = a + 1
+print(chr(b))
---
+++ Codeup/src_4/1046.py
@@ -0,0 +1,9 @@
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+print(a + b + c)
+
+d = (a + b + c) / 3
+print('%.1f' % (d))
